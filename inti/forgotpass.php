3ab0ce94ba810db25033a58089a0e38d484fa737
Lucas Chan <chilamlol@hotmail.com>
2022-03-25 20:33:36 +0800

    first commit

---
+++ inti/forgotpass.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+    <style> 
+     div {
+     width: 30%;
+     height: 50%;
+     padding: 30px;
+     box-shadow: 7px 7px 5px grey;
+     border:solid 2px grey;
+     margin-top: 120px;
+     }
+    </style>
+    </head>
+
+    <body>
+        <center>
+    <div>
+            <img src="logo.png" style="margin-bottom:28px;width:370px;height:70px;" />
+            <p style="width:100%; text-align:left; color:gray; font-size:16pt; font-family:Arial; font-weight:bold; height:40px; margin:0px;" >Forgot Password</p>
+            <p style="color:gray; font-size:10pt; font-family:Arial;text-align:left;">For any login issue please proceed to IT department, Inti College Subang to reset your password.<br><br><br>IT Department<br>03-5623 2800<br>support@newinti.edu.my</p>
+            
+            <form action="index.php">
+                <br>
+                  <input type="submit" value="Back" style="width:77px;height:25px;background-color:grey;color:white;border:0;margin-right:-276px;">
+                </form>  
+    </div>
+</center>
+    </body>
+</html>
